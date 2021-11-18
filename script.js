dd41e78cd04ef5393d7809817077f912c2dabc9c
triangle220 <92473111+triangle220@users.noreply.github.com>
2021-11-18 23:12:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let switchMode = document.getElementById("switchMode")
+
+switchMode.onclick = function () {
+    let theme = document.getElementById ("theme")
+    
+    if (theme.getAttribute("href") == "light-mode.css") {
+        theme.href = "dark-mode.css"
+    } else {
+        theme.href = "light-mode.css"
+    }
+}
+
+
+
+
+    
